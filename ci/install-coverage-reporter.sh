e771a72a079ae329b4da372af46f56e9a572a25c
ryotan <ryotan@ucb-jp.net>
2016-06-08 20:01:46 +0900

    use local directory.

--- ci/install-coverage-reporter.sh
+++ ci/install-coverage-reporter.sh
@@ -1,9 +1,27 @@
 #!/bin/bash
 
+if [[ -d "~/.jpm/jpm" ]]; then
+    sudo cp -r ~/.jpm/jpm /var/
+fi
+
+if [[ -d "~/.jpm/bin" ]]; then
+    sudo cp ~/.jpm/bin/* codacy-coverage-reporter} /usr/local/bin/
+fi
+
 if [[ ! $(builtin command -v jpm) ]]; then
   curl https://www.jpm4j.org/install/script | sudo sh
 fi
 
 if [[ ! $(builtin command -v codacy-coverage-reporter) ]]; then
   sudo jpm install com.codacy:codacy-coverage-reporter:assembly
+fi
+
+mkdir -p ~/.jpm/bin
+
+if [[ ! -d "~/.jpm/jpm" ]]; then
+  cp -r /var/jpm ~/.jpm/
+fi
+
+if [[ ! -d "~/.jpm/bin" ]]; then
+  cp /usr/local/bin/{jpm, codacy-coverage-reporter} ~/.jpm/bin/
 fi

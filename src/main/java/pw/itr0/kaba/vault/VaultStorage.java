40b8c884116e5dc3bcf00ac57286d0b8a70e5808
ryotan <ryotan@ucb-jp.net>
2016-06-13 17:59:58 +0900

    [wip] Revise shutdown hook to save multiple KeyStoreVaultStorage instances.

--- src/main/java/pw/itr0/kaba/vault/VaultStorage.java
+++ src/main/java/pw/itr0/kaba/vault/VaultStorage.java
@@ -4,6 +4,9 @@ import java.io.IOException;
 import java.nio.file.Path;
 import java.util.List;
 
+import pw.itr0.kaba.exception.IncorrectPasswordException;
+import pw.itr0.kaba.exception.InvalidPasswordFormatException;
+
 public interface VaultStorage<E> {
 
     void save(Path path, char[] password) throws IOException;
@@ -14,13 +17,13 @@ public interface VaultStorage<E> {
 
     void store(String name, Entry<? extends E> entry);
 
-    void store(String name, Entry<? extends E> entry, char[] password);
+    void store(String name, Entry<? extends E> entry, char[] password) throws InvalidPasswordFormatException;
 
-    byte[] retrieve(String name);
+    byte[] retrieve(String name) throws IncorrectPasswordException;
 
-    byte[] retrieve(String name, char[] password);
+    byte[] retrieve(String name, char[] password) throws IncorrectPasswordException;
 
-    void delete(String name);
+    void delete(String name) throws IncorrectPasswordException;
 
     interface Entry<E> {
         E item();
--- src/main/java/pw/itr0/kaba/vault/keystore/KeyStoreVaultStorage.java
+++ src/main/java/pw/itr0/kaba/vault/keystore/KeyStoreVaultStorage.java
@@ -3,6 +3,11 @@ package pw.itr0.kaba.vault.keystore;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.nio.channels.Channels;
+import java.nio.channels.ClosedChannelException;
+import java.nio.channels.FileChannel;
+import java.nio.channels.FileLock;
+import java.nio.channels.OverlappingFileLockException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.StandardOpenOption;
@@ -15,11 +20,19 @@ import java.security.UnrecoverableEntryException;
 import java.security.cert.CertificateException;
 import java.util.Collections;
 import java.util.List;
+import java.util.Objects;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadFactory;
+import java.util.stream.Stream;
 
 import pw.itr0.kaba.exception.ImplementationError;
 import pw.itr0.kaba.vault.VaultStorage;
 
 public class KeyStoreVaultStorage implements VaultStorage<KeyStore.Entry> {
+
+    private static final ConcurrentHashMap<Path, Thread> hooks = new ConcurrentHashMap<>();
+    private static final ThreadFactory tFactory = Executors.privilegedThreadFactory();
 
     private final KeyStore keyStore;
 
@@ -31,30 +44,59 @@ public class KeyStoreVaultStorage implements VaultStorage<KeyStore.Entry> {
             }
         } else {
             keyStore.load(null, password);
-            try (OutputStream file = Files.newOutputStream(path, StandardOpenOption.CREATE)) {
+            try (OutputStream file = Files.newOutputStream(path, StandardOpenOption.CREATE_NEW)) {
                 keyStore.store(file, password);
             }
         }
 
-        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+        Thread hook = tFactory.newThread(() -> {
+            System.out.println("Saving KeyStore file to path = " + path);
             try {
                 this.save(path, password);
             } catch (IOException e) {
                 throw new RuntimeException("Failed to save KeyStore file to " + path + ".", e);
             }
-        }));
+        });
+        hooks.compute(path, (p, orig) -> {
+            if (Objects.nonNull(orig)) {
+                Runtime.getRuntime().removeShutdownHook(orig);
+            }
+            Runtime.getRuntime().addShutdownHook(hook);
+            return hook;
+        });
     }
 
     @Override
     public void save(Path path, char[] password) throws IOException {
-        try (OutputStream file = Files.newOutputStream(path, StandardOpenOption.CREATE)) {
-            keyStore.store(file, password);
-        } catch (CertificateException e) {
-            throw new ImplementationError("This vault cannot store Certificates.", e);
-        } catch (NoSuchAlgorithmException e) {
-            throw new ImplementationError("Encryption/decryption algorithm must be statically implemented. This exception must not occur.", e);
-        } catch (KeyStoreException e) {
-            throw new IllegalStateException("KeyStore is not initialized.", e);
+
+        try (FileChannel channel = FileChannel.open(path, StandardOpenOption.WRITE, StandardOpenOption.TRUNCATE_EXISTING);
+                FileLock lock = channel.lock(0, Long.MAX_VALUE, false);
+                OutputStream file = Channels.newOutputStream(channel)) {
+            if (lock != null) {
+                try {
+                    keyStore.store(file, password);
+                } catch (CertificateException e) {
+                    throw new ImplementationError("This vault cannot store Certificates.", e);
+                } catch (NoSuchAlgorithmException e) {
+                    throw new ImplementationError("Algorithm must be statically implemented. This exception must not occur.", e);
+                } catch (KeyStoreException e) {
+                    throw new IllegalStateException("KeyStore is not initialized.", e);
+                }
+            }
+        } catch (ClosedChannelException ignore) {
+            StackTraceElement[] trace = ignore.getStackTrace();
+            boolean isIgnore = Stream.of(trace).anyMatch(elm ->{
+                String name = elm.getClassName();
+                String method = elm.getMethodName();
+                // Ignore ClosedChannelException on java.nio.channels.FileLock#release(), because KeyStore#store(OutputStream, char[]) closes OutputStream...
+                return "java.nio.channels.FileLock".equals(name) && "close".equals(method);
+            });
+
+            if (!isIgnore) {
+                throw ignore;
+            }
+        } catch (OverlappingFileLockException e) {
+            System.err.println("Failed to get lock on KeyStore file, and skipped saving file. path=[" + path + "].");
         }
     }
 
